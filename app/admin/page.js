bd8b015a41d72b1ed00dd0b081466ea9f105a008
RVWorld1 <getrvrepair@gmail.com>
2025-11-15 22:55:29 -0500

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,5 +1,7 @@
 export const dynamic = "force-dynamic";
 'use client';
+export const dynamic = "force-dynamic";
+
 import { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
